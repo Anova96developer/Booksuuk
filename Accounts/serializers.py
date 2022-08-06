831aa19f75082e5dfe6c2bf46a93a06acd29f12b
Sanusi <sanusiabubakr343@gmail.com>
2022-08-06 11:49:30 +0100

    mail testing added to user creation

---
+++ Accounts/serializers.py
@@ -0,0 +1,53 @@
+
+from  Accounts.models import User
+from rest_framework import serializers
+
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    username =serializers.CharField(max_length=25)
+    email = serializers.EmailField(max_length=80)
+    password  = serializers.CharField(min_length = 8,write_only=True)
+    is_verified = serializers.BooleanField(default = False)
+    
+    class Meta:
+        model= User
+        fields =['username','email','password','token','is_verified']
+    
+    
+    
+    def validate(self,attrs):
+        username_exists =User.objects.filter(username=attrs['username']).exists()
+        
+        if username_exists:
+            raise serializers.ValidationError("User with username exists")
+        
+        email_exists =User.objects.filter(email=attrs['email']).exists()
+        
+        if email_exists:
+            raise serializers.ValidationError("User with email exists")
+        
+        token_exists =User.objects.filter(token=attrs['token']).exists()
+        
+        if token_exists:
+            raise serializers.ValidationError("User with token exists")
+        
+        
+        return super().validate(attrs)
+    
+    
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data['username'],
+            email=validated_data['email'],
+            token= validated_data['token'],
+           
+
+        )
+            
+        user.set_password(validated_data['password'])      
+        
+        user.save()
+        
+        return user
+            
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,55 @@
+
+
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .import serializers
+import secrets
+from django.core.mail import send_mail # for email
 
 # Create your views here.
+
+class HelloAuthView(generics.GenericAPIView):
+        
+   
+    def get(self,request):
+        return Response(data={"message":"Hello Auth"},status=status.HTTP_200_OK)
+
+class UserCreateView(generics.GenericAPIView):
+    serializer_class = serializers.UserCreationSerializer
+    
+    
+    def post(self,request):
+
+        token = secrets.token_hex(30)
+
+        request.data.update({'token':token})
+
+        data = request.data
+         
+        
+        
+        serializer  = self.serializer_class(data=data)
+        
+        username = data['username']
+        reciever_email = data['email']
+
+
+        body  = f'Hi {username},\n You are seeing this message because you registered for Ebookify . Copy the code code below to verify your account   \n {token}'   
+
+        if serializer.is_valid():
+            serializer.save()
+
+
+            send_mail(
+           'Confirmation code for account verification',
+            body,
+            'Ebookify.app.com',
+            [reciever_email],
+              fail_silently=False,
+            )
+
+            return Response(data=serializer.data,status= status.HTTP_201_CREATED  )
+        
+        return  Response(data=serializer._errors,status=  status.HTTP_400_BAD_REQUEST)
