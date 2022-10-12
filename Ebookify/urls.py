ca79463539d5293abe1fc6c86cb158792ef7a320
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2022-10-12 16:05:08 +0100

    added test

--- Ebookify/urls.py
+++ Ebookify/urls.py
@@ -1,7 +1,5 @@
-
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -9,24 +7,28 @@ from drf_yasg import openapi
 # for images upload
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
-
-
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-   
-    path('api/v1/users/',include('account.urls')),
-    path('api/v1/books/', include('books.urls')),
-    path('api/v1/orders/', include('orders.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    
+    path("admin/", admin.site.urls),
+    path("api/v1/users/", include("account.urls")),
+    path("api/v1/books/", include("books.urls")),
+    path("api/v1/orders/", include("orders.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
-    path('api/v1/doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-   
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path(
+        "api/v1/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- account/tests.py
+++ account/tests.py
@@ -1,37 +1,51 @@
-from django.urls import reverse,path,include
+from urllib import response
+from click import password_option
+from django.urls import reverse, path, include
 from rest_framework import status
-from rest_framework.test import APITestCase,URLPatternsTestCase
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
 from .models import User
 
 
-#Todo test for User model
+# Todo test for User model
 # assert for token lenght and is_verified
 
-class RegisterTestCase (APITestCase):
-  
- 
-  def test_create_account(self) -> None:
-    
 
-    data = {
-      
-      "username" : "idan",
-      "email" : "idan@gmail.com",
-      "password":"password22",
-      "first_name":"hwhd",
-      "last_name":"dfjjfje",
-      "address":"4,Lagos Nigeria",
-      "phone_number":"+2348102045787",
-    }
+class RegisterTestCase(APITestCase):
+    def test_create_account(self) -> None:
 
-    url  = reverse('account:user-list')
-    response = self.client.post(url,data,format='json')
-    self.assertEqual(response.status_code,status.HTTP_201_CREATED)
-    self.assertLessEqual(len(User.objects.get().token),60)
-    self.assertEqual(User.objects.get().username,"idan")
-    
-    
+        data = {
+            "username": "idan",
+            "email": "idan@gmail.com",
+            "password": "password22",
+            "first_name": "hwhd",
+            "last_name": "dfjjfje",
+            "address": "4,Lagos Nigeria",
+            "phone_number": "+2348102045787",
+        }
 
- 
+        url = reverse("account:user-list")
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        self.assertLessEqual(len(User.objects.get().token), 60)
+        self.assertEqual(User.objects.get().username, "idan")
+        self.assertFalse(response.data.get("is_verified"))
+
+
+class LoginTestCase(APITestCase):
+    def setUp(self) -> None:
+        existing_data = {
+            "email": "active@gmail.com",
+            "username": "fagbo",
+            "password": "password123",
+            "is_verified": True,
+        }
+        user = User.objects.create_user(**existing_data)
+
+    def test_login_user(self):
+        login_data = {"email": "active@gmail.com", "password": "password123"}
+
+        url = reverse("account:user-login_user")
+        response = self.client.post(url, login_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
