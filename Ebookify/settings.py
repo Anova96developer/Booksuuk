fbcdd86112ac551f5ce349d401b1138b0a79dd77
SANUSI ABUBAKR <sanusiabubakr343@gmail.com>
2022-12-02 12:23:45 +0100

    added procfile

--- Ebookify/settings.py
+++ Ebookify/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # for Emails
@@ -198,6 +198,11 @@ DATE_INPUT_FORMATS = [
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join((BASE_DIR), "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
